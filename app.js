97712e6646dbfb3d9c1a7ff3adb499772d737741
cheatchansovoth <cheatchansovoth@gmail.com>
2023-04-08 18:56:13 +1200

    update

--- app.js
+++ app.js
@@ -9,6 +9,32 @@ app.get("/", (req, res) => {
 app.get("/test", (req, res) => {
   res.send("Hello World");
 });
+app.post("/create-checkout-session", async (req, res) => {
+  // const line_items = req.body.data.cart.map((item) => {
+  //   return {
+  //     price_data: {
+  //       currency: "usd",
+  //       product_data: {
+  //         name: item.name,
+  //         images: [item.image],
+  //         metadata: {
+  //           id: item.id,
+  //         },
+  //       },
+  //       unit_amount: item.price * 100,
+  //     },
+  //     quantity: 1,
+  //   };
+  // });
+  // const session = await stripe.checkout.sessions.create({
+  //   line_items,
+  //   mode: "payment",
+  //   success_url: `${YOUR_DOMAIN}/success.html`,
+  //   cancel_url: `${YOUR_DOMAIN}/`,
+  // });
+  res.json({ message: "Hello World from create-check-out-session" });
+  // res.send({ url: session.url });
+});
 app.post("/", (req, res) => {
   res.json({ message: "Hello World" });
 });
