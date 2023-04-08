f90b14edc83fe36782668154805ddffc5927adb7
cheatchansovoth <cheatchansovoth@gmail.com>
2023-04-08 19:02:58 +1200

    update post method

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const app = express();
-
+const stripe = require("stripe")(
+  "sk_test_51MtiOvJFziKWwKoooAjJuJXZT1E1ufaA3Ybkzs5uOd0nb9fYl7uwnacBh4sTKY2sGFzI9jtlr6jLpcCzk18BENa800t7PKjab1"
+);
 app.use(express.json());
 
 app.get("/", (req, res) => {
@@ -9,31 +11,31 @@ app.get("/", (req, res) => {
 app.get("/test", (req, res) => {
   res.send("Hello World");
 });
+const YOUR_DOMAIN = "http://localhost:5000";
 app.post("/create-checkout-session", async (req, res) => {
-  // const line_items = req.body.data.cart.map((item) => {
-  //   return {
-  //     price_data: {
-  //       currency: "usd",
-  //       product_data: {
-  //         name: item.name,
-  //         images: [item.image],
-  //         metadata: {
-  //           id: item.id,
-  //         },
-  //       },
-  //       unit_amount: item.price * 100,
-  //     },
-  //     quantity: 1,
-  //   };
-  // });
-  // const session = await stripe.checkout.sessions.create({
-  //   line_items,
-  //   mode: "payment",
-  //   success_url: `${YOUR_DOMAIN}/success.html`,
-  //   cancel_url: `${YOUR_DOMAIN}/`,
-  // });
-  res.json({ message: "Hello World from create-check-out-session" });
-  // res.send({ url: session.url });
+  const line_items = req.body.data.cart.map((item) => {
+    return {
+      price_data: {
+        currency: "usd",
+        product_data: {
+          name: item.name,
+          images: [item.image],
+          metadata: {
+            id: item.id,
+          },
+        },
+        unit_amount: item.price * 100,
+      },
+      quantity: 1,
+    };
+  });
+  const session = await stripe.checkout.sessions.create({
+    line_items,
+    mode: "payment",
+    success_url: `${YOUR_DOMAIN}/success.html`,
+    cancel_url: `${YOUR_DOMAIN}/`,
+  });
+  res.send({ url: session.url });
 });
 app.post("/", (req, res) => {
   res.json({ message: "Hello World" });
